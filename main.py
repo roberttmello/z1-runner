9bdb04b8d1829711dba21ca8999d7a1488851b80
Carlos <crobertdmelo@gmail.com>
2023-09-21 23:24:39 -0300

    Add - Basic collisions added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))  # Cria uma tela preta vazia (Display surface)
+screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Z1 RUNNER")
 clock = pygame.time.Clock()
 font = pygame.font.Font('font/pixeltype.ttf', 72)
@@ -18,11 +18,14 @@ snail_rectangle = snail_surface.get_rect(midbottom=(600, 480))
 player_surface = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom=(80, 480))
 
-while True:  # Loop principal do jogo
-    for event in pygame.event.get():  # Percorre uma lista de eventos
-        if event.type == pygame.QUIT:  # Se o evento for "Sair" o loop é encerrado
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        # if event.type == pygame.MOUSEMOTION:
+        #     if player_rectangle.collidepoint(event.pos):
+        #         print('collision')
 
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 480))
@@ -32,9 +35,10 @@ while True:  # Loop principal do jogo
     snail_rectangle.x -= 3
     if snail_rectangle.right <= 0:
         snail_rectangle.left = 1280
-    # player_rectangle.x += 3
-    # if player_rectangle.left >= 1280:
-    #     player_rectangle.right = 0
 
-    pygame.display.update()  # Atualiza a tela com os elementos necessários
-    clock.tick(60)  # Limita o loop principal a rodar no máximo há 60 fps
+    # mouse_position = pygame.mouse.get_pos()
+    # if player_rectangle.collidepoint(mouse_position):
+    #     print(pygame.mouse.get_pressed())
+
+    pygame.display.update()
+    clock.tick(60)
